aed9091f7d742e2bb493049a375e230968339300
morgan-mcnabb <morgan.mcnabb@protonmail.com>
2026-02-06 13:39:13 -0500

    initial project set up

---
+++ SuzerainSaveEditor.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace SuzerainSaveEditor.App.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ SuzerainSaveEditor.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SuzerainSaveEditor.App.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
